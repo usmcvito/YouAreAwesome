b8aafb922691250a948a2300fa9f0cf79423c58c
Vito <jegenovese@hotmail.com>
2023-09-03 07:09:05 -0400

    Version 1.0 - Layout settings

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "When the Genius Bar needs help, they call you everytime!"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 1)
+                .padding()
+            
+            HStack {
+                Button("Awesome") {
+                    // This is the action performed when the button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    // This is the action performed when the button is pressed
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .border(.purple, width: 3)
         }
-        .padding()
     }
 }
 
